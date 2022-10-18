1947f2d42f80dec38e9f053bd48cf9d99015aed9
Alex-Nelson-1 <113055690+Alex-Nelson-1@users.noreply.github.com>
2022-10-17 18:07:06 -0700

    Update main.cs

--- main.cs
+++ main.cs
@@ -8,20 +8,20 @@ class Program
 {
     public static void Main(string[] args)
     {
-   //Input
-   int length;
-   int width;
-   int area;
-   Console.WriteLine("This program calculates the area of a triangle");
-   Console.WriteLine("");
-   Console.WriteLine("Enter length in cm: ");
-   length = Convert.ToInt32(Console.ReadLine());
-   Console.WriteLine("Enter width in cm: ");
-   width = Convert.ToInt32(Console.ReadLine());
-   area = length * width / 2;
-   //Output
-   Console.WriteLine("");
-   Console.WriteLine("The area is: " + area + " cm²");
-   Console.WriteLine("\nDone");
+        //Input
+        int length;
+        int width;
+        int area;
+        Console.WriteLine("This program calculates the area of a triangle");
+        Console.WriteLine("");
+        Console.WriteLine("Enter length in cm: ");
+        length = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter width in cm: ");
+        width = Convert.ToInt32(Console.ReadLine());
+        area = length * width / 2;
+        //Output
+        Console.WriteLine("");
+        Console.WriteLine("The area is: " + area + " cm²");
+        Console.WriteLine("\nDone");
     }
 }
